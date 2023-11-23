bcb4b028c0cd90ea5e9bf31417473808df7739bf
Brskdd <yeetboi1229@gmail.com>
2023-11-23 02:24:58 -0500

    small update unimportant

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,11 @@ function App() {
     return () => clearInterval(fetchdata)
   }, [])
 
+
+  //create a nodemanager that FETCHes nodes.json and then iterates through the nodes to load them into the webpage
+
   return (
-    <div>
+    <div> 
       <Node id="1" name="appledisplay" type="singlenumber" posx1="0" posy1="0" posx2="15" posy2="5" color="ff0000" values="apples" />
       <Node id="2" name="orangedisplay" type="singlenumber" posx1="-2" posy1="6" posx2="8" posy2="8" color="ffff00" values="oranges" />
     </div>
