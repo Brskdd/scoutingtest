de9a635a64145e7b3e869aee0ad0b51741057261
Brskdd <yeetboi1229@gmail.com>
2023-12-03 22:42:02 -0500

    weekend update not much in code but lots of planning for where to take it

---
+++ client/src/components/nodetypes/SingleValue.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function SingleValue({inputs}) {
+    const val = inputs[0]
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default SingleValue
---
+++ client/src/components/nodetypes/VarVsVar.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+function VarVsVar({inputs, outputs}) {
+    const var1 = inputs[0]
+    const var2 = inputs[1]
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default VarVsVar
