12a10d15369c1b575a45dd5e1ecf410ef0f5cfc1
Brskdd <yeetboi1229@gmail.com>
2023-12-05 23:27:36 -0500

    began work on envvars system

--- client/src/components/nodetypes/SingleValue.js
+++ client/src/components/nodetypes/SingleValue.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 function SingleValue({inputs}) {
-    const val = inputs[0]
+    const val = String(inputs[0][0]).padStart(4, "0") + String(inputs[0][1]).padStart(4, "0") + inputs[0][2]
+
+    //ok so it gets an envar like 00590006TeleopHighCones find a way to get that key in the envvars for the bank
     return (
         <div>
-            <p>test text</p>
+            <p>envvar input is {val}</p>
         </div>
     )
 }
