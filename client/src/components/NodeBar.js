3075b7e3eba18a08f9677324d82213ebdd472687
BroskiDood29 <yeetboi1229+1@gmail.com>
2023-12-31 10:21:51 -0500

    began work on node creation ui

--- client/src/components/NodeBar.js
+++ client/src/components/NodeBar.js
@@ -2,15 +2,25 @@ import React, { useState, useEffect } from "react";
 
 function NodeBar() {
     const [mode, setmode] = useState("open") //once youre done change this to closed so its not open on startup
+    const [divwidth, setdivwidth] = useState("50px")
+    let styledata= {
+        width: divwidth
+    }
     function switchmode() {
+        
         if (mode == "open") {
-            setmode("closed")
+            setmode("closed") 
+            setdivwidth("50px")
         } else {
             setmode("open")
+            setdivwidth("300px")
         }
     }
+    
+    
     return (
-        <div className="p-4 fixed w-10 h-5/6 right-0 bottom-0 border-2 border-white">
+        
+        <div className="p-4 fixed h-5/6 bottom-0 border-2 border-white right-0" onClick={switchmode} style={styledata}>
             <p className="text-white">sidebar</p>
         </div>
     )
